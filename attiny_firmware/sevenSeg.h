7e3e6839d3eaa5add1ef2a5ca1a703d2ae2492cf
Antony Chan <antony.sigma@gmail.com>
2013-07-25 15:42:29 +0800

    Extract PID control code and place into separate library

--- attiny_firmware/sevenSeg.h
+++ attiny_firmware/sevenSeg.h
@@ -14,12 +14,6 @@ const int dataPin = PIN_A7;
 ///Digit reset pin
 const int resetPin = PIN_A6;
 
-///Torque control pin
-const int spinPin = PIN_A5;
-///Speed input pin (PIN_A1)
-const int speedPin = 1;
-
-
 void DisplayReset();
 void convert2Digits(long num100);
 void DisplayDigits();
