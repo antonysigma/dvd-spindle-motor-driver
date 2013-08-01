9e43df0acce3214a08f3804f21a68bd7df08e02f
Antony Chan <antony.sigma@gmail.com>
2013-08-01 11:12:44 +0800

    Change pins in firmware to accomodate changes in the schematics

--- attiny_firmware/sevenSeg.h
+++ attiny_firmware/sevenSeg.h
@@ -6,11 +6,11 @@
 #include "core_pins.h"
 
 //Pin connected to ST_CP of 74HC595
-const int latchPin = PIN_B0;
+const int latchPin = PIN_A7;
 //Pin connected to SH_CP of 74HC595
 const int clockPin = PIN_B1;
 ////Pin connected to DS of 74HC595
-const int dataPin = PIN_A7;
+const int dataPin = PIN_B0;
 ///Digit reset pin
 const int resetPin = PIN_A6;
 
